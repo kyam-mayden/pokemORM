371f1279d23d6b051adbe928367ac4cd23681c81
Kyam Harris <kyam.lh@googlemail.com>
2020-10-27 08:41:42 +0000

    Rename leader table to be consistent

--- database/migrations/2020_10_27_081100_create_gym_leader_table.php
+++ database/migrations/2020_10_27_081100_create_gym_leader_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGymLeadersTable extends Migration
+class CreateGymLeaderTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateGymLeadersTable extends Migration
      */
     public function up()
     {
-        Schema::create('gym_leaders', function (Blueprint $table) {
+        Schema::create('gym_leader', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('gym');
             $table->integer('leader');
