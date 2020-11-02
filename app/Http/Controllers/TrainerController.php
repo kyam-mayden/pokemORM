b6428fd6881a78a3bfd1907efbbf0a55d89034b0
Kyam Harris <kyam.lh@googlemail.com>
2020-11-02 16:08:28 +0000

    Add update method

--- app/Http/Controllers/TrainerController.php
+++ app/Http/Controllers/TrainerController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Trainer;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TrainerController extends Controller
 {
-    //
+    public function update(Request $request, $id, $firstName, $secondName)
+    {
+        $trainer = DB::select('SELECT * FROM trainer WHERE id = ?', [$id]);
+
+        if (empty($trainer)) {
+            throw new Exception();
+        }
+
+        DB::update(
+            'UPDATE trainer
+                SET first_name = ?,
+                    second_name = ?
+              WHERE id = ?',
+            [$firstName, $secondName, $id]
+        );
+
+        $trainer = DB::select('SELECT * FROM trainer WHERE id = ?', [$id]);
+
+//        $trainer = Trainer::findOrFail($id);
+//        $trainer->first_name = $firstName;
+//        $trainer->second_name = $secondName;
+//
+//        $trainer->save();
+
+//        $trainer = Trainer::where('id', $id)
+//            ->update(
+//                [
+//                    'first_name' => $firstName,
+//                    'second_name' => $secondName,
+//                ]
+//            );
+
+        return response()->json($trainer);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
+use App\Http\Controllers\TrainerController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// http://pokemorm.test/trainer/20/update/ross/kemp
+Route::get('/trainer/{id}/update/{new_first_name}/{new_second_name}', [TrainerController::class, 'update']);
