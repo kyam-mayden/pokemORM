6ab14030ba3163d2e466d5fd1144deacd3e9cba1
Kyam Harris <kyam.lh@googlemail.com>
2021-09-20 09:41:11 +0100

    Split out query for readability

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -49,8 +49,8 @@ class BattleController extends Controller
                     t1.second_name AS trainer_1_second_name,
                     s1.name AS trainer_1_favourite,
                     pt1.name AS trainer_1_favourite_type,
-                    t2.id AS trainer_2_id,
 
+                    t2.id AS trainer_2_id,
                     t2.first_name AS trainer_2_first_name,
                     t2.second_name AS trainer_2_second_name,
                     s2.name AS trainer_2_favourite,
@@ -62,10 +62,13 @@ class BattleController extends Controller
                     INNER JOIN trainer AS t1 ON battle.trainer_1 = t1.id
                     INNER JOIN trainer AS t2 ON battle.trainer_2 = t2.id
                     INNER JOIN trainer AS winner ON battle.winner_id = winner.id
+
                     INNER JOIN pokemon AS p1 ON t1.favourite_pokemon = p1.id
                     INNER JOIN pokemon AS p2 ON t2.favourite_pokemon = p2.id
+
 					INNER JOIN species AS s1 ON p1.species_id = s1.id
                     INNER JOIN species AS s2 ON p2.species_id = s2.id
+
                     INNER JOIN pokemon_type AS pt1 ON s1.primary_type = pt1.id
                     INNER JOIN pokemon_type AS pt2 ON s2.primary_type = pt2.id'
         );
