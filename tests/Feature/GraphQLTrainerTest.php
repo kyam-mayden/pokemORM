8c112498f19aaa494c3d9410e32492f0ca32c696
marty-crane <kyam.harris@mayden.co.uk>
2021-11-23 12:51:32 +0000

    Test we can get the trainers trained pokemon

--- tests/Feature/GraphQLTrainerTest.php
+++ tests/Feature/GraphQLTrainerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Pokemon;
 use App\Trainer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -48,5 +49,44 @@ class GraphQLTrainerTest extends TestCase
         ];
 
         $this->assertEquals($expected, $data['trainer']);
+    }
+
+
+    public function testWeGetTrainedPokemon(): void
+    {
+        $response = $this->post('api/graphql/trainer', [
+            'query' => '{
+                 trainer (id: 1) {
+                      trained_pokemon {
+                          id
+                          species {
+                              name
+                              pokedex_number
+                              primary_type
+                              secondary_type
+                          }
+                      }
+                  }
+                }',
+        ]);
+
+        $data = json_decode($response->getContent(), true)['data'];
+        $expected = Pokemon::where('trainer_id', 1)->get();
+
+        $this->assertCount($expected->count(), $data['trainer']['trained_pokemon']);
+
+        $expected = $expected->map(function ($trained) {
+            return [
+                'id' => $trained->id,
+                'species' => [
+                    'name' => $trained->species->name,
+                    'pokedex_number' => $trained->species->pokedex_number,
+                    'primary_type' => $trained->species->primaryType->name,
+                    'secondary_type' => $trained->species->secondaryType?->name,
+                ],
+            ];
+        })->toArray();
+
+        $this->assertEquals($expected, $data['trainer']['trained_pokemon']);
     }
 }
