e9da2056ee0f9007e6ecfd95a1fa9d16c0731b40
marty-crane <kyam.harris@mayden.co.uk>
2021-11-23 11:51:11 +0000

    Test we get the trainers favourite type

--- tests/Feature/GraphQLTrainerTest.php
+++ tests/Feature/GraphQLTrainerTest.php
@@ -8,18 +8,14 @@ use Tests\TestCase;
 
 class GraphQLTrainerTest extends TestCase
 {
-    use RefreshDatabase;
-
     public function testWeGetTrainerDetails(): void
     {
-        $this->seed();
         $response = $this->post('api/graphql/trainer', [
             'query' => '{
                   trainer (id: 1) {
                       id
                       first_name
                       second_name
-                      favourite_type
                   }
                 }',
         ]);
@@ -30,6 +26,24 @@ class GraphQLTrainerTest extends TestCase
             'id' => '1',
             'first_name' => 'Ash',
             'second_name' => 'Ketchum',
+        ];
+
+        $this->assertEquals($expected, $data['trainer']);
+    }
+
+    public function testWeGetTrainerFavouriteType(): void
+    {
+        $response = $this->post('api/graphql/trainer', [
+            'query' => '{
+                  trainer (id: 1) {
+                      favourite_type
+                  }
+                }',
+        ]);
+
+        $data = json_decode($response->getContent(), true)['data'];
+
+        $expected = [
             'favourite_type' => 'Rock',
         ];
 
